13f090bb45f167320f7f4e232b5c4c8f9f085914
Arpit Kushwaha <98214210+arpitkushwaha90@users.noreply.github.com>
2026-02-19 18:59:29 +0530

    Create DAY-19_Missing Element in Range.java

---
+++ February/DAY-19_Missing Element in Range.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ArrayList<Integer> missingRange(int[] arr, int low, int high) {
+        ArrayList<Integer> result = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+        for (int i = low; i <= high; i++) {
+            if (!set.contains(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
