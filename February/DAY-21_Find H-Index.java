84ea7f1ce4bfbc0261fb36c1ef23210cff7d450d
Arpit Kushwaha <98214210+arpitkushwaha90@users.noreply.github.com>
2026-02-21 19:45:59 +0530

    Create DAY-21_Find H-Index.java

---
+++ February/DAY-21_Find H-Index.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        // code here
+        int n = citations.length;
+        int maxCitation = 0;
+        for (int citation : citations) {
+            maxCitation = Math.max(maxCitation, citation);
+        }
+        int[] count = new int[maxCitation + 2];
+        for (int citation : citations) {
+            count[0]++;
+            count[citation + 1]--;
+        }
+        for (int i = 1; i < count.length; i++) {
+            count[i] += count[i - 1];
+        }
+        for (int i = count.length - 1; i >= 0; i--) {
+            if (count[i] >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
