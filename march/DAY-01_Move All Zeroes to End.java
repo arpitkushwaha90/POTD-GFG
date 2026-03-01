bae95544afeb07a8c3d81bc4bb18fd32d803930c
Arpit Kushwaha <98214210+arpitkushwaha90@users.noreply.github.com>
2026-03-01 20:20:05 +0530

    Create DAY-01_Move All Zeroes to End.java

---
+++ march/DAY-01_Move All Zeroes to End.java
@@ -0,0 +1,16 @@
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int nonZeroIndex = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[nonZeroIndex] = arr[i];
+                nonZeroIndex++;
+            }
+        }
+        
+        for (int i = nonZeroIndex; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+    }
+}
