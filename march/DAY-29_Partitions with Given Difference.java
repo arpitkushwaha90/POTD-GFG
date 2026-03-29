100a8d318dfd635c06e40cbbeea883ee3bf49710
Arpit Kushwaha <98214210+arpitkushwaha90@users.noreply.github.com>
2026-03-29 20:13:50 +0530

    Create DAY-29_Partitions with Given Difference.java

---
+++ march/DAY-29_Partitions with Given Difference.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countPartitions(int[] arr, int diff) {
+        int sum = 0;
+        for (int num : arr) sum += num;
+        if (sum < diff || (sum + diff) % 2 != 0) return 0;
+        int target = (sum + diff) / 2;
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int num : arr) {
+            for (int j = target; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[target];
+    }
+}
