57d1837ea3c31eef77bd65de1eaf6e6674607b05
Arpit Kushwaha <98214210+arpitkushwaha90@users.noreply.github.com>
2026-04-10 21:23:29 +0530

    Create DAY-10_Sorted subsequence of size 3.java

---
+++ April/DAY-10_Sorted subsequence of size 3.java
@@ -0,0 +1,41 @@
+class Solution {
+    public ArrayList<Integer> find3Numbers(int[] arr) {
+        int n = arr.length;
+        int[] smaller = new int[n];
+        int[] greater = new int[n];
+
+        smaller[0] = -1;
+        int minIndex = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] <= arr[minIndex]) {
+                minIndex = i;
+                smaller[i] = -1;
+            } else {
+                smaller[i] = minIndex;
+            }
+        }
+
+        greater[n - 1] = -1;
+        int maxIndex = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= arr[maxIndex]) {
+                maxIndex = i;
+                greater[i] = -1;
+            } else {
+                greater[i] = maxIndex;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (smaller[i] != -1 && greater[i] != -1) {
+                ArrayList<Integer> res = new ArrayList<>();
+                res.add(arr[smaller[i]]);
+                res.add(arr[i]);
+                res.add(arr[greater[i]]);
+                return res;
+            }
+        }
+
+        return new ArrayList<>();
+    }
+}
