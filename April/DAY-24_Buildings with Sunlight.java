0f224c9fad2bf6ab2e33686d3e083c1339ed8d69
Arpit Kushwaha <98214210+arpitkushwaha90@users.noreply.github.com>
2026-04-24 21:28:13 +0530

    Create DAY-24_Buildings with Sunlight.java

---
+++ April/DAY-24_Buildings with Sunlight.java
@@ -0,0 +1,18 @@
+//Buildings with Sunlight
+
+class Solution {
+
+    public int visibleBuildings(int[] arr) {
+        int count = 1;
+        int maxHeight = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] >= maxHeight) {
+                count++;
+                maxHeight = arr[i];
+            }
+        }
+
+        return count;
+    }
+}
