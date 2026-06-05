517a25e258be3b488176de4f114229fd562ce585
Arpit Kushwaha <98214210+arpitkushwaha90@users.noreply.github.com>
2026-06-05 22:32:51 +0530

    Create DAY-05_Lexicographically smallest after removing k.java

---
+++ JUNE/DAY-05_Lexicographically smallest after removing k.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String lexicographicallySmallest(String s, int k) {
+        int n = s.length();
+        if ((n & (n - 1)) == 0) {
+            k /= 2;
+        } else {
+            k *= 2;
+        }
+        if (k >= n) return "-1";
+        StringBuilder st = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            while (k > 0 && st.length() > 0 &&
+                   st.charAt(st.length() - 1) > ch) {
+                st.deleteCharAt(st.length() - 1);
+                k--;
+            }
+            st.append(ch);
+        }
+        while (k > 0 && st.length() > 0) {
+            st.deleteCharAt(st.length() - 1);
+            k--;
+        }
+        if (st.length() == 0) return "-1";
+        return st.toString();
+    }
+}
