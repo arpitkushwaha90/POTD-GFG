a4757e654cfa5527220fe5f337103f8cf89a35f4
Arpit Kushwaha <98214210+arpitkushwaha90@users.noreply.github.com>
2026-07-02 12:23:19 +0530

    Create DAY-02_Check Subset sum divisible by k.java

---
+++ JULY/DAY-02_Check Subset sum divisible by k.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean divisibleByK(int[] arr, int k) {
+
+        boolean[] dp = new boolean[k];
+
+        for (int num : arr) {
+
+            boolean[] next = dp.clone();
+
+            // Subset containing only current element
+            next[num % k] = true;
+
+            // Extend previous subsets
+            for (int r = 0; r < k; r++) {
+                if (dp[r]) {
+                    next[(r + num) % k] = true;
+                }
+            }
+
+            dp = next;
+
+            if (dp[0]) return true;
+        }
+
+        return false;
+    }
+}
